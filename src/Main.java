26375fb4a68dd48cc5d9a74f5d7a97adb43e5f61
kristynf <thetruthandsoul@gmail.com>
2020-01-24 12:34:39 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -24,15 +24,15 @@ public class Main {
 
         int dice1=(int)(Math.random()*6+1);
         int dice2=(int)(Math.random()*6+1);
-        int sum= dice1 + dice2;
+        int sum = dice1 + dice2;
         int[] values = new int[1];  // Holds the highest values
 
         System.out.println("Roll: total = " + sum);
 
-        public int sum (){
-            int sum = 0;
+       {
+            sum = 0;
             for (int i = 0;i < values.length;i++) {
-                sum = ((sum + values[i]));
+                sum = (( sum + values[i]));
 
             }
 
