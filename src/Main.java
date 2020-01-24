baf1a4579fa7989ce428e95238eaa332b8571161
kristynf <thetruthandsoul@gmail.com>
2020-01-24 13:05:06 -0500

    updated

--- src/Main.java
+++ src/Main.java
@@ -17,56 +17,59 @@ public class Main {
 
     }
 
-    public static HighestValue welcome() {
+    public static void welcome() {
         System.out.println("Welcome to my Dice Game");{
         }
         String response;
 
-        int dice1=(int)(Math.random()*6+1);
-        int dice2=(int)(Math.random()*6+1);
-        int sum = dice1 + dice2;
-        int[] values = new int[1];  // Holds the highest values
 
-        System.out.println("Roll: total = " + sum);
 
-       {
-            sum = 0;
-            for (int i = 0;i < values.length;i++) {
-                sum = (( sum + values[i]));
+
+            int sum = 0;
+            while (sum <= 100) {
+                int dice1 = (int) (Math.random() * 6 + 1);
+                int dice2 = (int) (Math.random() * 6 + 1);
+                int sum = dice1 + dice2;
+                int total
+
+                System.out.println("Your rolls:" + dice1 +"  " + dice2);
+                System.out.println("Roll: total = " + sum);
+
+
 
             }
+                do {
+
+                    if (sum > 100) {
+                        System.out.println("Congratulations, You Win!");
+                    } else if (dice1 == 1 && dice2 == 1) {
+                        System.out.println("SNAKE EYES!" + sum + 25);
+                    } else if (dice1 == 1 && dice2 == 2) {
+                        System.out.println("You Lose!");
+                    } else if (dice1 == 1 && dice2 == 3) {
+                        System.out.println("You Lose!");
+                    } else if (dice1 == 1 && dice2 == 4) {
+                        System.out.println("You Lose!");
+                    } else if (dice1 == 1 && dice2 == 5) {
+                        System.out.println("You Lose!");
+                    } else if (dice1 == 1 && dice2 == 6) {
+                        System.out.println("You Lose!");
+                    }
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Your score is: " + sum);
+                    System.out.println("Your total is: " + values);
+                    System.out.println("Do you want to continue: y/n");
+                    response = sc.nextLine();
+                }
 
 
-        if (sum > 100){
-            System.out.println("Congratulations, You Win!");}
-        else if (dice1==1 && dice2==1){
-            System.out.println("SNAKE EYES!" + sum + 25);}
-        else if(dice1==1 && dice2==2){
-            System.out.println("You Lose!");}
-
-        else if(dice1==1 && dice2==3){
-            System.out.println("You Lose!");}
-
-        else if(dice1==1 && dice2==4){
-            System.out.println("You Lose!");}
-
-        else if(dice1==1 && dice2==5){
-            System.out.println("You Lose!");}
-
-        else if(dice1==1 && dice2==6){
-            System.out.println("You Lose!");}
-            Scanner sc = new Scanner(System.in);
-        System.out.println("Your score is: " + sum);
-        System.out.println("Your total is: " + values);
-        System.out.println("Do you want to continue: y/n");
-           response = sc.nextLine();}
-
-        while (response.equalsIgnoreCase("n"));
-            System.out.println("Thanks for playing!");
-
-        }
+        while (response.equalsIgnoreCase("y"));
 
 
 
+    }
 
-        }
+
+
+
+        }}}
