acf9ce0464aa87a8046a62e43a189f67f50feb4f
kristynf <thetruthandsoul@gmail.com>
2020-01-24 12:23:19 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,45 @@
+import com.sun.source.tree.ContinueTree;
+
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-
-
         //variables
         int dice1;
         int dice2;
 
-
-        //Call the welcome method
+       //Call the welcome method
         welcome();
 
         //fetch random numbers
 
-
-        /*
-         * **************************************************************
-         *welcome method
-         *welcome user
-         *no parameters
-         *no return
-         ****************************************************************
-         */
     }
+
     public static void welcome() {
         System.out.println("Welcome to my Dice Game");{
         }
+        String response;
 
         int dice1=(int)(Math.random()*6+1);
         int dice2=(int)(Math.random()*6+1);
         int sum= dice1 + dice2;
-        int[] values = new int[];  // Holds the highest values
+        int[] values = new int[1];  // Holds the highest values
 
         System.out.println("Roll: total = " + sum);
 
-        public int highestValue() {
+        public int sum (){
             int sum = 0;
             for (int i = 0;i < values.length;i++) {
                 sum = ((sum + values[i]));
+
             }
-            return sum;
-        }
 
 
-
-        if (dice1==1 && dice2==1){
-            System.out.println("SNAKE EYES!" + sum + 25)}
+        if (sum > 100){
+            System.out.println("Congratulations, You Win!");}
+        else if (dice1==1 && dice2==1){
+            System.out.println("SNAKE EYES!" + sum + 25);}
         else if(dice1==1 && dice2==2){
             System.out.println("You Lose!");}
 
@@ -62,13 +54,18 @@ public class Main {
 
         else if(dice1==1 && dice2==6){
             System.out.println("You Lose!");}
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Your score is: " + sum);
+        System.out.println("Your total is: " + values);
+        System.out.println("Do you want to continue: y/n");
+           response = sc.nextLine();}
 
-
-        while(sum <100 && answer == "yes"){
+        while (response.equalsIgnoreCase("n"));
+            System.out.println("Thanks for playing!");
 
         }
 
 
 
 
-        }}
+        }
